0d11b52be394c4b3c6961179f8c1e7453485456b
xus <xus206@hotmail.com>
2018-02-15 14:47:03 +0100

    User registration + mailing

--- classes/class.ilQuickSignUpPluginGUI.php
+++ classes/class.ilQuickSignUpPluginGUI.php
@@ -70,6 +70,11 @@ class ilQuickSignUpPluginGUI extends ilPageComponentPluginGUI
 	 */
 	var $ui_renderer;
 
+	/**
+	 * @var ilSetting
+	 */
+	var $settings;
+
 	//we probably can create a PHP unique id
 	var $form_login_id = "form_login_pl_qs";
 	var $form_register_id = "form_register_pl_qs";
@@ -89,6 +94,7 @@ class ilQuickSignUpPluginGUI extends ilPageComponentPluginGUI
 		$this->lng = $DIC->language();
 		$this->ui_factory = $DIC->ui()->factory();
 		$this->ui_renderer = $DIC->ui()->renderer();
+		$this->settings = $DIC->settings();
 
 		$this->globals_init = true;
 
@@ -220,15 +226,15 @@ class ilQuickSignUpPluginGUI extends ilPageComponentPluginGUI
 		$legacy_content = "";
 		switch ($current_status)
 		{
+			/* //Decided not to show this message.
 			case ilAuthStatus::STATUS_AUTHENTICATED:
-				//todo: lang var
 				$auth_result = array(
 					"status" => "ok",
 					"html" => "welcome_back"
 				);
 				echo json_encode($auth_result);
 				exit;
-
+			*/
 			case ilAuthStatus::STATUS_AUTHENTICATION_FAILED:
 				//todo remove inline css and use the ilias sendFailure css
 				$css = "background-color:red; color:white; margin:10px 0; padding:10px;";
@@ -275,12 +281,41 @@ class ilQuickSignUpPluginGUI extends ilPageComponentPluginGUI
 	{
 		//$this->modal_id = $_GET['replaceSignal'];
 
-		$this->ctrl->setParameter($this, "replaceSignal", $this->modal_id);
+		//$this->ctrl->setParameter($this, "replaceSignal", $this->modal_id);
+		$this->ctrl->setParameter($this, "replaceSignal", $_GET['replaceSignal']);
 
 		$this->setTabOption(self::MD_REGISTER_VIEW);
 
-		$legacy_content = $this->initFormRegister()->getHTML();
-		$legacy_content .= $this->appendJS($this->getRegisterValidationURL(), "form_register_modal_plugin");
+		include_once './Services/Authentication/classes/class.ilAuthStatus.php';
+		$status = ilAuthStatus::getInstance();
+
+		$current_status = $status->getStatus();
+		$legacy_content = "";
+
+		switch($current_status)
+		{
+			case ilAuthStatus::STATUS_AUTHENTICATION_FAILED:
+				ilLoggerFactory::getRootLogger()->debug("Authentication failed");
+				//todo remove inline css and use the ilias sendFailure css
+				$css = "background-color:red; color:white; margin:10px 0; padding:10px;";
+				$legacy_content = $this->getNavigation();
+				$legacy_content .= "<div style='" . $css . "'>" . $status->getTranslatedReason() . "</div>" . $this->getRegisterForm()->getHTML();
+				$legacy_content .= $this->appendJS($this->getRegisterUrl(), "form_register_modal_plugin");
+				$legacy_content .= " ".$this->getPasswordAssistance();
+				$auth_result = array(
+					"status" => "ko",
+					"html" => $legacy_content
+				);
+				echo json_encode($auth_result);
+				exit;
+		}
+
+		//get default form.
+		if($current_status !=ilAuthStatus::STATUS_AUTHENTICATED && $legacy_content == "")
+		{
+			$legacy_content = $this->getRegisterForm()->getHTML();
+			$legacy_content .= $this->appendJS($this->getRegisterValidationURL(), "form_register_modal_plugin");
+		}
 
 		$modal_content = $this->getNavigation();
 		$modal_content .= $legacy_content;
@@ -289,7 +324,8 @@ class ilQuickSignUpPluginGUI extends ilPageComponentPluginGUI
 
 		// Build a submit button (action button) for the modal footer
 		$form_id = "form_register_modal_plugin";
-		$submit = $this->ui_factory->button()->primary('Submit', '#')
+
+		$submit = $this->ui_factory->button()->primary('Register', '#')
 			->withOnLoadCode(function($id) use ($form_id) {
 				return "$('#{$id}').click(function() { $('#{$form_id}').submit(); return false; });";
 			});
@@ -577,16 +613,13 @@ class ilQuickSignUpPluginGUI extends ilPageComponentPluginGUI
 			return $terms_text;
 		}
 	}
+
 	/**
 	 * @return string with the password assistance links
 	 */
 	public function getPasswordAssistance()
 	{
-		global $DIC;
-
-		$il_setting = $DIC->settings();
-
-		if ($il_setting->get("password_assistance"))
+		if ($this->settings->get("password_assistance"))
 		{
 			$link_pass = $this->ui_factory->button()->shy($this->lng->txt("forgot_password"), $this->ctrl->getLinkTarget($this, "jumpToPasswordAssistance"));
 			$link_name = $this->ui_factory->button()->shy($this->lng->txt("forgot_username"), $this->ctrl->getLinkTarget($this, "jumpToNameAssistance"));
@@ -691,7 +724,9 @@ class ilQuickSignUpPluginGUI extends ilPageComponentPluginGUI
 						}
 					 },
 					error: function(result) {
-						$('.modal-body').html('Something is wrong!');
+					    console.log('error->'+result.responseText);
+					    /*todo: remove the register/login button + lang var*/
+						$('.modal-body').html('Something is wrong!->'+result.responseText);
 					 }
 				});
 			});
@@ -714,7 +749,6 @@ class ilQuickSignUpPluginGUI extends ilPageComponentPluginGUI
 		include_once("Services/Form/classes/class.ilPropertyFormGUI.php");
 		$form = new ilPropertyFormGUI();
 		$form->setFormAction($this->ctrl->getFormAction($this));
-		$form->setName("formregister");
 		$form->setId("register_modal_plugin");
 		$form->setShowTopButtons(false);
 
@@ -739,14 +773,17 @@ class ilQuickSignUpPluginGUI extends ilPageComponentPluginGUI
 		$pi->setRequired(true);
 		$form->addItem($pi);
 
+		//TODO remove this after fix the action button JS.
+		$form->addCommandButton('saveRegistration', $this->lng->txt('Register'));
+
+
 		return $form;
 	}
 
+	//TODO: The mail IS NOT VALIDATED HERE.
+	//TODO: clean the duplicate code. (resolution array for instance)
 	function saveRegistration()
 	{
-		global $DIC;
-		$ilSetting = $DIC->settings();
-
 		//need this for the email domains.
 		$registration_settings = new ilRegistrationSettings();
 
@@ -786,60 +823,48 @@ class ilQuickSignUpPluginGUI extends ilPageComponentPluginGUI
 					$mail_obj = $form->getItemByPostVar('usr_email');
 					$mail_obj->setAlert(sprintf($this->lng->txt("reg_email_domains"),
 						implode(", ", $domains)));
-					ilLoggerFactory::getRootLogger()->debug("MAIL NOT VALIDATED");
 					$form_valid = false;
 				}
 			}
 		}
 
 		$error_lng_var = '';
-		//todo: Should we take care about this auto generated pass?
 		if(
 			//!$this->registration_settings->passwordGenerationEnabled() &&
 			!ilUtil::isPasswordValidForUserContext($form->getInput('usr_password'), $form->getInput('username'), $error_lng_var)
 		)
 		{
-			ilLoggerFactory::getRootLogger()->debug("PASSWORD not valid for this User Context");
 			$passwd_obj = $form->getItemByPostVar('usr_password');
 			$passwd_obj->setAlert($this->lng->txt($error_lng_var));
 			$form_valid = false;
 		}
 
 		//role
-		include_once 'Services/Registration/classes/class.ilRegistrationEmailRoleAssignments.php';
-		$registration_role_assignments = new ilRegistrationRoleAssignments();
-
-
-/* WORKING HERE no valid role*/
-/*
-		ilLoggerFactory::getRootLogger()->debug("Email to get the role => ".$form->getInput("usr_email"));
-		$valid_role = (int)$registration_role_assignments->getRoleByEmail($form->getInput("usr_email"));
+		//Last role from available roles for registration.(first one can be Administrator)
+		$role_data = array_pop(ilObjRole::_lookupRegisterAllowed());
+		$valid_role = $role_data['id'];
 
 		// no valid role could be determined
 		if (!$valid_role)
 		{
-			ilLoggerFactory::getRootLogger()->debug("No valid role");
-			ilUtil::sendInfo($this->lng->txt("registration_no_valid_role"));
 			$form_valid = false;
 		}
-*/
+
 		// validate username
 		$login_obj = $form->getItemByPostVar('username');
 		$login = $form->getInput("username");
 		if (!ilUtil::isLogin($login))
 		{
-			ilLoggerFactory::getRootLogger()->debug("IS NOT LOGIN login =>".$login);
 			$login_obj->setAlert($this->lng->txt("login_invalid"));
 			$form_valid = false;
 		}
 		else if (ilObjUser::_loginExists($login))
 		{
-			ilLoggerFactory::getRootLogger()->debug("LOGIN EXISTS");
 			$login_obj->setAlert($this->lng->txt("login_exists"));
 			$form_valid = false;
 		}
-		else if ((int)$ilSetting->get('allow_change_loginname') &&
-			(int)$ilSetting->get('reuse_of_loginnames') == 0 &&
+		else if ((int)$this->settings->get('allow_change_loginname') &&
+			(int)$this->settings->get('reuse_of_loginnames') == 0 &&
 			ilObjUser::_doesLoginnameExistInHistory($login))
 		{
 			ilLoggerFactory::getRootLogger()->debug("LOGIN EXISTS 2");
@@ -847,26 +872,89 @@ class ilQuickSignUpPluginGUI extends ilPageComponentPluginGUI
 			$form_valid = false;
 		}
 
+
+
+
 		//resolution
-		if(!$form_valid) {
-			//working here
-			ilLoggerFactory::getRootLogger()->debug("FORM NO VALID");
-			//return input not valid
-			$this->register();
-			//echo $this->lng->txt('form_input_not_valid');
+		if(!$form_valid)
+		{
+			$form->setValuesByPost();
+			$html = $this->getNavigation();
+			//todo centralize the CSS for errors in the CSS file.
+			if(!$valid_role){
+				$html .= "<div id='quick_sign_up_modal_error' style='background-color:red;color:white;'>".$this->lng->txt("registration_no_valid_role")."</div>";
+			}
+			$html .= $form->getHTML();
+			$auth_result = array(
+				"status" => "ko",
+				"html" => $html
+			);
+			echo json_encode($auth_result);
 			exit;
+		}
+		else
+		{
+			$user_data = array(
+				"username" => $form->getInput("username"),
+				"email" => $form->getInput("usr_email"),
+				"password" => $form->getInput("usr_password")
+			);
+			//create user
+			if($this->createUser($valid_role, $user_data))
+			{
+				//return status ok, html empty
+				$auth_result = array(
+					"status" => "ko",
+					"html" => "user can be registered, change this KO to OK"
+				);
+
+				echo json_encode($auth_result);
+				exit;
+			}
+			else
+			{
+				$html = $this->getNavigation();
+				//todo centralize the CSS for errors in the CSS file.
+				$html .= "<div id='quick_sign_up_modal_error' style='background-color:red;color:white;'>".$this->lng->txt("registration_can_not_register")."</div>";
+				$html .= $form->getHTML();
+				$auth_result = array(
+					"status" => "ko",
+					"html" => $html
+				);
+				echo json_encode($auth_result);
+				exit;
+			}
+
+		}
+	}
+
+	protected function createUser($a_role, $a_user_data)
+	{
+		// something went wrong with the form validation
+		if(!$a_role)
+		{
+			global $ilias;
+			$ilias->raiseError("Invalid role selection in registration".
+				", IP: ".$_SERVER["REMOTE_ADDR"], $ilias->error_obj->FATAL);
+		}
+
+		$user_object = new ilObjUser();
+
+		include_once("./Services/User/classes/class.ilUserProfile.php");
+		$up = new ilUserProfile();
+		$up->setMode(ilUserProfile::MODE_REGISTRATION);
+
+		$user_object->setLogin($a_user_data["username"]);
+		$user_object->setEmail($a_user_data["email"]);
+		$user_object->setPasswd($a_user_data["password"]);
+
+		if($user_object->create()) {
+			$user_object->saveAsNew();
+			//send mail notification
+			$this->sendRegistrationEmail($user_object);
+			return true;
 		} else {
-			ilLoggerFactory::getRootLogger()->debug("FORM VALID");
-
-			$user_created = $this->createUser($valid_role);
-			//todo get the current language for this.
-			//$this->distributeMails($user_created, $form->getInput("usr_language"));
-			$this->distributeMails($user_created, "en");
-
-			//TODO login + redirect to complete your profile?
-			//$this->login($user_created);
-			echo "registered";
-			exit;
+			return false;
 		}
 	}
 
@@ -892,8 +980,45 @@ class ilQuickSignUpPluginGUI extends ilPageComponentPluginGUI
 			$custom_tpl->setCurrentBlock("terms");
 			$custom_tpl->setVariable("CONTENT", $content);
 			$custom_tpl->parseCurrentBlock();
+			$custom_tpl->show();
 		}
 		//avoid redirect
 		exit;
+	}
+
+	/**
+	 * @param $a_user_object ilObjUser
+	 */
+	public function sendRegistrationEmail($a_user_object)
+	{
+		// try individual account mail in user administration
+		include_once("Services/Mail/classes/class.ilAccountMail.php");
+		include_once './Services/User/classes/class.ilObjUserFolder.php';
+		include_once "Services/Mail/classes/class.ilMimeMail.php";
+
+		$senderFactory = $GLOBALS["DIC"]["mail.mime.sender.factory"];
+
+		$mmail = new ilMimeMail();
+		$mmail->From($senderFactory->system());
+		$mmail->To($a_user_object->getEmail());
+
+		// mail subject
+		$subject = $this->lng->txt("reg_mail_subject");
+
+		// mail body
+		$body = $this->lng->txt("reg_mail_body_salutation")." ".$a_user_object->getLogin().",\n\n".
+			$this->lng->txt("reg_mail_body_text1")."\n\n".
+			$this->lng->txt("reg_mail_body_text2")."\n".
+			ILIAS_HTTP_PATH."/login.php?client_id=".CLIENT_ID."\n";
+		$body .= $this->lng->txt("login").": ".$a_user_object->getLogin()."\n";
+
+		$body.= "\n";
+
+		$body .= ($this->lng->txt("reg_mail_body_text3")."\n\r");
+		$body .= $a_user_object->getProfileAsString($this->lng);
+		$mmail->Subject($subject);
+		$mmail->Body($body);
+		$mmail->Send();
+
 	}
 }
